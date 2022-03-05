52da3ded6ee7a353ce526d8b82fdcdd0033797c7
Ismail Andaloussi <oneigcy@gmail.com>
2022-03-05 16:23:38 +0100

    Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,30 +9,21 @@ use Auth;
 
 class UserController extends Controller
 {
-    public function select(Request $request){
-        $user_id = lib::filter($request['user_id']);
-        if(Auth::check()){
-            if((Auth::user()->role >= 3) || lib::access(Auth::user()->id, 'user_select')){
-                $users = User::whereId($user_id)->get();
-                if(count($users)){
-                    $user = [
-                        'admin' => $users[0]->role >= 2 ? true : false,
-                        'owner' => $users[0]->role >= 3 ? true : false,
-                        'id' => $users[0]->id,
-                        'role' => $users[0]->role,
-                        'avatar' => $users[0]->avatar,
-                        'name' => $users[0]->name,
-                        'email' => $users[0]->email,
-                        'phone' => $users[0]->phone,
-                        'permission' => Permission::select('id', 'name', 'access')->where('user_id', $users[0]->id)->get(),
-                    ];
-                    return response()->json($user);
-                }
-                return response()->json(['status' => 404]);
-            }
-            return response()->json(['status' => 403]);
+    private static function get($user_id){
+        if($user = User::whereId($user_id)->first()){
+            $response = [
+                'admin' => $user->role >= 2 ? true : false,
+                'owner' => $user->role >= 3 ? true : false,
+                'id' => $user->id,
+                'role' => $user->role,
+                'name' => $user->name,
+                'email' => $user->email,
+                'phone' => $user->phone,
+                'permission' => Permission::select('id', 'name', 'access')->where('user_id', $user->id)->get(),
+            ];
+            return $response;
         }
-        return response()->json(['status' => 401]);
+        return false;
     }
 
     public function all(){
@@ -47,7 +38,6 @@ class UserController extends Controller
                         'owner' => $owner,
                         'id' => $user->id,
                         'role' => $user->role,
-                        'avatar' => $user->avatar,
                         'name' => $user->name,
                         'email' => $user->email,
                         'phone' => $user->phone,
@@ -55,6 +45,20 @@ class UserController extends Controller
                     ]);
                 }
                 return response()->json($response);
+            }
+            return response()->json(['status' => 403]);
+        }
+        return response()->json(['status' => 401]);
+    }
+
+    public function select(Request $request){
+        $user_id = lib::filter($request['user_id']);
+        if(Auth::check()){
+            if((Auth::user()->role >= 3) || lib::access(Auth::user()->id, 'user_select')){
+                if($user = User::whereId($user_id)->first()){
+                    return response()->json(self::get($user->id));
+                }
+                return response()->json(['status' => 404]);
             }
             return response()->json(['status' => 403]);
         }
@@ -83,12 +87,11 @@ class UserController extends Controller
     public function edit(Request $request){
         if(Auth::check()){
             if((Auth::user()->role >= 3) || lib::access(Auth::user()->id, 'user_edit')){
-                $user_id = lib::filter($request['user_id']);
-                $name = lib::filter($request['name']);
-                $email = lib::filter($request['email']);
-                $phone = lib::filter($request['phone']);
-                $role = lib::filter($request['role']);
-                $role = lib::filter($request['role']);
+                $user_id = $request['user_id'];
+                $name = $request['name'];
+                $email = $request['email'];
+                $phone = $request['phone'];
+                $role = $request['role'];
                 if(count(User::whereId($user_id)->get())){
                     User::whereId($user_id)->update([
                         'name' => $name,
